e81231a2207487a7a878da92c66864f3e98aeca6
Kartik Pawar <knpawar789@gmail.com>
2025-05-19 16:01:01 +0530

    ui: landing page and auth model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Footer from "@/components/landing/Footer";
+import HeroSection from "@/components/landing/HeroSection";
+import Navbar from "@/components/landing/Navbar";
+import Pricing from "@/components/landing/Pricing";
+import React, { Fragment } from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Fragment>
+      <Navbar />
+      <HeroSection />
+      <Pricing />
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default App;
