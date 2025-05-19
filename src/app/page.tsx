2f27c728d1c195f55ae7f32e5a64a36b50c18a78
Kartik Pawar <knpawar789@gmail.com>
2025-05-19 17:20:58 +0530

    feat: user auth sign-in flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,18 @@ import HeroSection from "@/components/landing/HeroSection";
 import Navbar from "@/components/landing/Navbar";
 import Pricing from "@/components/landing/Pricing";
 import React, { Fragment } from "react";
+import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 
-function App() {
+export default async function Home() {
+  const session: CustomSession | null = await getServerSession(authOptions);
+
   return (
     <Fragment>
-      <Navbar />
+      <Navbar user={session?.user} />
       <HeroSection />
-      <Pricing />
+      <Pricing user={session?.user} />
       <Footer />
     </Fragment>
   );
 }
-
-export default App;
