bab579c532068f7f63d28fda891b65b9a3250652
Kartik Pawar <knpawar789@gmail.com>
2025-05-19 19:55:20 +0530

    feat: user coins

---
+++ src/actions/fetchActions.ts
@@ -0,0 +1,17 @@
+import { prisma } from "@/lib/db.config";
+import { unstable_cache } from "next/cache";
+
+export const getUserCoins = unstable_cache(
+  async (userId: string) => {
+    return await prisma.user.findUnique({
+      where: {
+        id: Number(userId),
+      },
+      select: {
+        coins: true,
+      },
+    });
+  },
+  ["userCoins"],
+  { revalidate: 60 * 60, tags: ["userCoins"] }
+);
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,12 +3,15 @@ import DashNav from "@/components/dashboard/DashNav";
 import React from "react";
 import { authOptions, CustomSession } from "../api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
+import { getUserCoins } from "@/actions/fetchActions";
 
 export default async function Dashboard() {
   const session: CustomSession | null = await getServerSession(authOptions);
+
+  const userCoins = await getUserCoins(session?.user?.id!);
   return (
     <div>
-      <DashNav user={session?.user!} userCoins={null} />
+      <DashNav user={session?.user!} userCoins={userCoins} />
     </div>
   );
 }
